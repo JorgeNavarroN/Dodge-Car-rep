78a079631eb57099ee26b344a76c1ecc1ea5645c
JorgeNavarroN <jnnavarroxd@gmail.com>
2023-04-13 18:52:43 -0500

    dodge-car v0.1.2

--- juego.py
+++ juego.py
@@ -43,11 +43,9 @@ linea_centro_alto = 100
 linea_centro_x = (ventana.get_width() - linea_centro_ancho) // 2
 linea_centro_y = 25
 
-autosRivales = []
 autoRival_ancho = 50
 autoRival_alto = 80
 autoRival_y = -80
-
 autoRival = pygame.Rect(random.randint(borde_izquierdo.right, borde_derecho.left - autoRival_ancho),
                             autoRival_y, autoRival_ancho, autoRival_alto)
 
@@ -57,6 +55,12 @@ moto_y = -80
 moto = pygame.Rect(random.randint(borde_izquierdo.right, borde_derecho.left - moto_ancho),
                             moto_y, moto_ancho, moto_alto)
 
+camion_ancho = 70
+camion_alto = 120
+camion_y = -120
+camion = pygame.Rect(random.randint(borde_izquierdo.right, borde_derecho.left - camion_ancho),
+                     camion_y, camion_ancho, camion_alto)
+
 for i in range(4):
     linea_centro = pygame.Rect(linea_centro_x, linea_centro_y, linea_centro_ancho, linea_centro_alto)
     lineas_centro.append(linea_centro)
@@ -64,10 +68,12 @@ for i in range(4):
     
 auto_anterior_bajo = False
 moto_anterior_bajo = False
+camion_anterior_bajo = False
 
 velocidad_auto = 3
 velocidad_auto_giro = 0.8
 velocidad_moto = 1
+velocidad_camion = 1
 velocidad_autoRival = 2
 running = True
 paused = False
@@ -90,11 +96,14 @@ while running:
                             autoRival_y, autoRival_ancho, autoRival_alto)
                 moto = pygame.Rect(random.randint(borde_izquierdo.right, borde_derecho.left - moto_ancho),
                             moto_y, moto_ancho, moto_alto)
+                camion = pygame.Rect(random.randint(borde_izquierdo.right, borde_derecho.left - camion_ancho),
+                     camion_y, camion_ancho, camion_alto)
                 score = -1
                 velocidad_auto = 3
                 velocidad_auto_giro = 0.8
                 velocidad_autoRival = 2
                 velocidad_moto = 1
+                velocidad_camion = 1
                 paused = not paused
             
     ventana.fill((0, 0, 0))
@@ -112,7 +121,7 @@ while running:
     
     auto = pygame.Rect(auto_x, auto_y, auto_ancho, auto_alto)
     
-    if not paused and (auto.colliderect(autoRival) or auto.colliderect(moto)):
+    if not paused and (auto.colliderect(autoRival) or auto.colliderect(moto) or auto.colliderect(camion)):
         paused = True
         scores["score"] = score
         print(scores["score"])
@@ -125,6 +134,7 @@ while running:
         velocidad_auto_giro = 0
         velocidad_autoRival = 0
         velocidad_moto = 0
+        velocidad_camion = 0
     keys = pygame.key.get_pressed()
     
     if auto.left > borde_izquierdo.right:
@@ -150,6 +160,14 @@ while running:
     if moto.top >= ventana.get_height():
         moto_anterior_bajo = True
     
+    if camion_anterior_bajo:
+        camion = pygame.Rect(random.randint(borde_izquierdo.right, borde_derecho.left - camion_ancho),
+                     camion_y, camion_ancho, camion_alto)
+        camion_anterior_bajo = False
+    
+    if camion.top >= ventana.get_height():
+        camion_anterior_bajo = True
+    
     
     pygame.time.delay(3)
     pygame.draw.rect(ventana, (255, 43, 123), autoRival)
@@ -164,7 +182,26 @@ while running:
             velocidad_auto_giro = 1
             velocidad_autoRival = 3
             velocidad_moto = 2
+    
+    if score >= 60:
+        pygame.draw.rect(ventana, (10, 252, 74), camion)
+        camion.move_ip(0, velocidad_camion)
         
+    if score >= 80:
+        if not paused:
+            velocidad_auto = 5
+            velocidad_auto_giro = 1.2
+            velocidad_autoRival = 4
+            velocidad_moto = 3
+            velocidad_camion = 2
+    if score >= 100:
+        if not paused:
+            velocidad_auto = 6
+            velocidad_auto_giro = 1.4
+            velocidad_autoRival = 5
+            velocidad_moto = 4
+            velocidad_camion = 3
+    
     pygame.time.delay(1)
     pygame.draw.rect(ventana, (255, 0, 0), auto)
     pygame.display.flip()
